5a974ab9e1752a696adb300327dabd7cd666ed5b
Izar Mula Soria <izar19mula97@gmail.com>
2021-12-08 21:28:16 +0100

    Change Player Model + Solve Movement

--- OpChads/Assets/Scripts/Character/ThirdPersonMovement.cs
+++ OpChads/Assets/Scripts/Character/ThirdPersonMovement.cs
@@ -8,11 +8,17 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float speed = 6f;
 
+    private bool groundedPlayer;
+
     public float turnSmoothTime = 0.1f;
 
     float turnSmoothVelocity;
     
     public Transform cam;
+
+    private float gravityValue = -9.81f;
+
+    private Vector3 playerVelocity;
 
     // Update is called once per frame
     void Update()
@@ -21,14 +27,22 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
         if(direction.magnitude >= 0.1f) {
 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir * Time.deltaTime * speed);
         }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
